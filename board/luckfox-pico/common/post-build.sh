5a0fd4d25ce5c8f6ffdd9c77e902b2d7f7197955
Huy Duong <huy.duong@ologn.tech>
2024-11-08 11:23:55 +0700

    Add rockchip kmpp and rockit driver modules

--- board/luckfox-pico/common/post-build.sh
+++ board/luckfox-pico/common/post-build.sh
@@ -2,5 +2,12 @@
 
 BOARD_DIR=$(dirname "$0")
 
-install -m 0755 $BOARD_DIR/S20loadmodules $TARGET_DIR/etc/init.d/
-install -m 0755 $BOARD_DIR/S49usbgadget $TARGET_DIR/etc/init.d/
+install -D -m 0644 dl/luckfox-pico/git/sysdrv/drv_ko/kmpp/release_kmpp_rv1106_arm/mpp_vcodec.ko \
+        $TARGET_DIR/lib/modules/5.10.160/extra/mpp_vcodec.ko
+install -D -m 0644 dl/luckfox-pico/git/sysdrv/drv_ko/rockit/release_rockit-ko_rv1106_arm/rockit.ko \
+        $TARGET_DIR/lib/modules/5.10.160/extra/rockit.ko
+install -D -m 0644 dl/luckfox-pico/git/sysdrv/drv_ko/rockit/release_rockit-ko_rv1106_arm/hpmcu_wrap.bin \
+        $TARGET_DIR/lib/firmware/hpmcu_wrap.bin
+
+install -m 0755 $BOARD_DIR/S20loadmodules $TARGET_DIR/etc/init.d/S20loadmodules
+install -m 0755 $BOARD_DIR/S49usbgadget $TARGET_DIR/etc/init.d/S49usbgadget
