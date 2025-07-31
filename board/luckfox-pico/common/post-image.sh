2674d77febbfce06a5db9ee839d0e7e546cbe62a
Huy Duong <huy.duong@ologn.tech>
2025-07-31 11:18:27 +0700

    Support Luckfox Pico Ultra (W) (#10)
    * Support board luckfox-pico-ultra-w
    
    * Rename ultra-w to ultra
    
    * Update luckfox pico ultra configs
    
    * Use common post-image.sh for Luckfox Pico Ultra
    
    * Upgrade rockchip-rkbin to latest version
    
    * Use separate uboot-env.txt for each board
    
    * Update uboot-env.txt for Luckfox Pico Ultra
    
    * Update uboot defconfig for Luckfox Pico Ultra
    
    * Add init script to resize rootfs on first boot
    
    ---------
    
    Co-authored-by: Loc Phan <loc.phan@ologn.tech>

--- board/luckfox-pico/common/post-image.sh
+++ board/luckfox-pico/common/post-image.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
-mv -f $BINARIES_DIR/rootfs.ubi $BINARIES_DIR/rootfs.img 2>/dev/null
+if grep -q "BR2_TARGET_ROOTFS_UBI=y" $BR2_CONFIG; then
+    mv -f $BINARIES_DIR/rootfs.ubi $BINARIES_DIR/rootfs.img 2>/dev/null
+    rm -f $BINARIES_DIR/rootfs.ubifs
+elif grep -q "BR2_TARGET_ROOTFS_EXT2=y" $BR2_CONFIG; then
+    mv -f $BINARIES_DIR/rootfs.ext2 $BINARIES_DIR/rootfs.img 2>/dev/null
+    rm -f $BINARIES_DIR/rootfs.ext4
+fi
+
 mv -f $BINARIES_DIR/uboot-env.bin $BINARIES_DIR/env.img 2>/dev/null
 rm -f $BINARIES_DIR/*.dtb
-rm -f $BINARIES_DIR/rootfs.ubifs
